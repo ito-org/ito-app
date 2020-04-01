eac6dc035f90384b6bd0bc8c6ae626e17a6b8b30
calmandniceperson <mko.mko31@gmail.com>
2020-04-01 21:12:54 +0200

    Remove template comment

--- App.tsx
+++ App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {
   SafeAreaView,
