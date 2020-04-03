f43a2cd621e8238ce679b6d63598ec03a3975ec3
haveyaseen <haveyaread@gmail.com>
2020-04-03 20:01:00 +0200

    call download API every 60 minutes

--- App/App.tsx
+++ App/App.tsx
@@ -22,19 +22,29 @@ import {BluetoothNotification} from './screens/BluetoothNotification';
 import {SymptomInfo} from './screens/SymptomInfo';
 import {DataUpload} from './screens/DataUpload';
 import {ConfirmationCode} from './screens/ConfirmationCode';
+import {LongPressGestureHandler} from 'react-native-gesture-handler';
 
 const Stack = createStackNavigator();
 
 declare var global: {HermesInternal: null | {}};
 
 const REGENERATE_UUID_MINUTES = 30;
+const POLL_DOWNLOAD_API_MINUTES = 60;
+
+const DOWNLOAD_API_BASE_URI = 'https://api.bandemic.app/v0';
 
 const App = () => {
   const [uuid, setUuid] = useState(v4());
+  const [downloadedUuids, setDownloadedUuids] = useState([]);
   useEffect(() => {
     BackgroundTimer.runBackgroundTimer(() => {
       setUuid(v4());
     }, REGENERATE_UUID_MINUTES * 60 * 1000);
+    BackgroundTimer.runBackgroundTimer(() => {
+      fetch(`${DOWNLOAD_API_BASE_URI}/cases?uuid=${v4()}`).then((result) => {
+        console.log(result);
+      });
+    }, POLL_DOWNLOAD_API_MINUTES * 60 * 1000);
     BackgroundTimer.start();
   }, []);
   return (
@@ -61,7 +71,7 @@ const App = () => {
           backgroundColor: '#444',
           padding: 4,
         }}>
-        Your ID: {uuid}
+        {uuid} ({downloadedUuids.length} others)
       </Text>
     </NavigationContainer>
   );
