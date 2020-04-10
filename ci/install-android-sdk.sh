8da9be3666cf23bd166c387ef47e40359bfe8d95
Jörn Buchwald <git@joernbuchwald.com>
2020-04-10 19:58:52 +0200

    add android build workflow

---
+++ ci/install-android-sdk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget --quiet --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip
+unzip -d android-sdk-linux android-sdk.zip
+echo y | android-sdk-linux/tools/bin/sdkmanager "platforms;android-${ANDROID_COMPILE_SDK}" >/dev/null
+echo y | android-sdk-linux/tools/bin/sdkmanager "platform-tools" >/dev/null
+echo y | android-sdk-linux/tools/bin/sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}" >/dev/null
+echo y | android-sdk-linux/tools/bin/sdkmanager "ndk;${ANDROID_NDK}" >/dev/null
+
+# temporarily disable checking for EPIPE error and use yes to accept all licenses
+set +o pipefail
+yes | android-sdk-linux/tools/bin/sdkmanager --licenses
+set -o pipefail
