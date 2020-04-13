ce16023171bf0b7ae57c778afb08d043da58a5b7
haveyaseen <haveyaread@gmail.com>
2020-04-14 00:41:07 +0200

    adopt mint styling for infected? screen

--- App/screens/Endangerment.tsx
+++ App/screens/Endangerment.tsx
@@ -3,46 +3,61 @@ import {View, StyleSheet, Text, TouchableWithoutFeedback} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from 'App/App';
+import AlphaNotice from '../components/AlphaNotice';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: 'hsl(224, 71%, 58%)',
+    backgroundColor: '#ffffff',
+  },
+  logoWrapper: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    position: 'relative',
+  },
+  alphaNoticeRoot: {
+    position: 'absolute',
+    top: 12,
+    left: 48,
+    padding: 0,
+  },
+  alphaNoticeText: {
+    fontSize: 14,
+    lineHeight: 14,
   },
   logo: {
-    color: 'white',
+    color: '#7dc6b6',
     fontSize: 32,
     textAlign: 'center',
     fontFamily: 'Righteous-Regular',
     marginBottom: 16,
   },
   header: {
-    fontSize: 15,
+    fontSize: 18,
     padding: 5,
     paddingTop: 64,
     paddingBottom: 50,
-    color: 'white',
+    color: '#595959',
     fontFamily: 'Ubuntu-R',
     lineHeight: 26,
-    letterSpacing: 1,
   },
   arrowRightIcon: {
     right: 16,
     top: 16,
     fontSize: 20,
-    color: 'hsl(224, 71%, 58%)',
+    color: '#2c2c2c',
     position: 'absolute',
   },
   buttonSymptoms: {
     borderRadius: 8,
-    backgroundColor: 'white',
-    padding: 16,
+    backgroundColor: '#91e6d3',
+    padding: 20,
     marginBottom: 32,
     fontFamily: 'Ubuntu-R',
   },
   buttonSymptomsTitle: {
-    color: '#4770e0',
+    color: '#2c2c2c',
     letterSpacing: 1,
     textTransform: 'uppercase',
     fontSize: 14,
@@ -52,12 +67,12 @@ const styles = StyleSheet.create({
   },
   buttonTested: {
     borderRadius: 8,
-    backgroundColor: 'white',
-    padding: 16,
+    backgroundColor: '#91e6d3',
+    padding: 20,
     fontFamily: 'Ubuntu-R',
   },
   buttonTestedTitle: {
-    color: '#4770e0',
+    color: '#2c2c2c',
     letterSpacing: 1,
     textTransform: 'uppercase',
     fontSize: 14,
@@ -78,10 +93,16 @@ export function Endangerment({
 }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.logo}>ito</Text>
+      <View style={styles.logoWrapper}>
+        <Text style={styles.logo}>ito</Text>
+        <AlphaNotice
+          rootStyle={styles.alphaNoticeRoot}
+          textStyle={styles.alphaNoticeText}
+        />
+      </View>
       <Text style={styles.header}>
-        Tell us if you have symptoms or you{'\n'}
-        already have a positive test result
+        Tell us if you have symptoms or if you{'\n'}
+        have a positive test result
       </Text>
       <TouchableWithoutFeedback
         onPress={() => navigation.navigate('SymptomInfo')}>
@@ -103,7 +124,7 @@ export function Endangerment({
           <Icon name="arrow-right" size={18} style={styles.arrowRightIcon} />
           <Text>
             If you got a positive result from your doctor or authorities please
-            let us know to help everybody else stay healthy
+            let us know to help everybody else stay healthy.
           </Text>
         </View>
       </TouchableWithoutFeedback>
