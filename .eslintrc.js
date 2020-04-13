f0e3453819bb82152ea7539d3c6361cffd2ac3ae
haveyaseen <haveyaread@gmail.com>
2020-04-13 13:27:24 +0200

    add stricter rules for typescript, run pretty-quick pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
 };
