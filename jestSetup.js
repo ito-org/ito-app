1e25030a33ce8e0700ba3fdf8971ab5c0ca59eb1
haveyaseen <haveyaread@gmail.com>
2020-05-04 18:22:52 +0200

    Fix JS test workflow (#143)
    * import correct App module
    
    * update jest related dependencies, mock async storage and static files
    
    * enable app rendering CI
    
    * run app rendering step before building APK
    
    * ignore scripts in CI to prevent Snyk from applying patches
    
    * call jest.useFakeTimers() to mock setTimeout
    
    * do not ignore npm scripts after installing

---
+++ jestSetup.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
+jest.useFakeTimers();
